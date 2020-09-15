8bb5b3884b26d97484d76f9085a9db3d9c958b12
kbu715 <kbu715@gmail.com>
2020-09-15 19:26:57 +0900

    header

---
+++ src/Components/views/Header/Header.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="header">
+              <div className="header-logo">
+                <a href="#">CARGO</a>
+              </div>
+              <div className="header-menu">
+                <ul>
+                  <li>
+                    <a href="#">TEMPLATES</a>
+                  </li>
+                  <li>
+                    <a href="#">SITES</a>
+                  </li>
+                  <li>
+                    <a href="#">SELECTIONS</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="header-social-media">
+                <ul>
+                  <li>
+                    <a href="https://twitter.com">
+                      <i className="fab fa-twitter"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.instagram.com/">
+                      <i className="fab fa-instagram"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.facebook.com/">
+                      <i className="fab fa-facebook-f"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="dropdown">
+                <button className="dropbtn">
+                  <i className="fas fa-bars"></i>
+                </button>
+                <div className="dropdown-content">
+                  <a href="#">TEMPLATES</a>
+                  <a href="#">SITES</a>
+                  <a href="#">SELECTIONS</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header;
